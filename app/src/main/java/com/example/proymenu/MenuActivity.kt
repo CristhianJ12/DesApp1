aed866dcdf17e7288582dfc1176c32bfb7ac870b
String <75049277@continental.edu.pe>
2024-09-25 20:27:23 -0500

    Pantallas creadas y menu funcional

---
+++ app/src/main/java/com/example/proymenu/MenuActivity.kt
@@ -0,0 +1,29 @@
+package com.example.proymenu
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+            val button1=findViewById<AppCompatButton>(R.id.retos_btn)
+            button1.setOnClickListener {
+                val lanzar = Intent(this, RetosActivity::class.java)
+                startActivity(lanzar)
+            }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
