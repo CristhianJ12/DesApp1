2f5c47b76f531b990113fdecddf626dcfc926167
String <75049277@continental.edu.pe>
2024-09-25 21:55:07 -0500

    Correccion del menu, ya funciona

--- app/src/main/java/com/example/proymenu/MenuActivity.kt
+++ app/src/main/java/com/example/proymenu/MenuActivity.kt
@@ -2,6 +2,7 @@ package com.example.proymenu
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -14,16 +15,26 @@ class MenuActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
 
-            val button1=findViewById<AppCompatButton>(R.id.retos_btn)
-            button1.setOnClickListener {
-                val lanzar = Intent(this, RetosActivity::class.java)
-                startActivity(lanzar)
-            }
+        val button1=findViewById<AppCompatButton>(R.id.retos_btn)
+        val button2=findViewById<AppCompatButton>(R.id.applib_btn)
+        button1.setOnClickListener {toRetos()}
+        button2.setOnClickListener {toAppLib()}
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun toAppLib() {
+        Log.d("MenuActivity", "Botón de Librería presionado")
+        val lanzar1 = Intent(this, LibreriaActivity::class.java)
+        startActivity(lanzar1)
+    }
+
+    private fun toRetos() {
+        val lanzar2 = Intent(this, RetosActivity::class.java)
+        startActivity(lanzar2)
     }
 }
